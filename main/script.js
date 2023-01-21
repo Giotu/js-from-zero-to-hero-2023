bd5879548eb8b09f9e3bb0c4dfbfc20ef96fa81c
Giotu <evilalex1998@mail.ru>
2023-01-21 19:46:18 +0300

    feat: solved problem with close burger menu by mouse click on items menu

--- main/script.js
+++ main/script.js
@@ -1,6 +1,13 @@
 const btn = document.querySelector('.header__button');
 const nav = document.querySelector('.header__menu');
+const itemMenu = document.querySelectorAll('.header__list-item');
 
 btn.addEventListener('click', () => {
     nav.classList.toggle('menu-open');
-})
+});
+
+itemMenu.forEach(item => {
+    item.addEventListener('click', () => {
+        nav.classList.toggle('menu-open');
+    })
+});
